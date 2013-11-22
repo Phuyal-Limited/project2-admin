4ec8b0203aa8a4de34e2e7cb51c25f8e6b6b4493
Genese Software Solutions <genese@genese>
2013-11-22 14:21:18 +0545

    loading image and jquery file

---
+++ assets/js/jq.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	//ajax login
+	$("#submit-button").click(function () {
+ 		var user = $("#user").val();
+		var pass = $("#pass").val();
+		
+		if(user==''){
+			$("#msg").text('User Required!');
+			return false;
+		}
+		if(pass==''){
+			$("#msg").text('Password Required!');
+			return false;
+		}
+		
+		$.ajax({
+			
+			url:'login_entry',
+			type:'POST',
+			data: {
+				name: user,
+				pass: pass 
+			},
+			beforeSend: function(){
+				$("#load").html('<img src="http://localhost/admin.nepalinn.com/assets/images/loading.gif" height="20px" width="20px">');
+			},
+			success: function(response){
+				$("#load").hide('');
+				$("#msg").html('');
+				if(response==='successful'){
+					window.location.replace("home");
+				}else{
+					$("#msg").text(response);
+				}
+			},
+		});
+		return false;
+  	});
+
+
+});
