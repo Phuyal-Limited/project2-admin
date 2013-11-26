c1c8909d8a8fac6dcde8e190342e85da555f9a55
Bidur <bidur_subedi@yahoo.com>
2013-11-26 21:43:03 +0545

    Check room availability in a range of date
    public function get_status_on_range($roomID,$fromDate,$toDate); function
    created and tested.

--- application/models/rooms.php
+++ application/models/rooms.php
@@ -39,7 +39,32 @@ class Rooms extends CI_Model{
 		return $result;
 	}
 
-
+	/*Function to obtain the status of room for a range of dates
+	  Room ID and the date range  to check status should be supplied
+	  By: Bidur Subedi
+	  Nov 26, 2013 */
+	public function get_status_on_range($roomID,$fromDate,$toDate){
+		//Obtain all booking with that room ID
+		$this->db->select('booking_id');
+		$this->db->where('room_id',$roomID);
+		$booking_id=$this->db->get('booking_room');
+		$booking_id=$booking_id->result();
+		$book_id = array();
+		foreach ($booking_id as $value) {
+			array_push($book_id, $value->booking_id);	
+		}
+		if ($book_id == array())
+			return 0;
+		$this->db->select('status');
+		$this->db->where_in('booking_id',$book_id);
+		$this->db->where("checkin_date < '$fromDate' AND checkout_date > '$fromDate'");
+		$this->db->or_where("checkin_date < '$toDate' AND checkout_date > '$toDate'");
+		$result=$this->db->get('booking')->result();
+		if($result == array())
+			return 0;
+		$result=$result[0]->status;
+		return $result;
+	}
 }
 
 ?>
