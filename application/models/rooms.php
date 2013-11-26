a4ab284259f8d7a4a0fbef87f41636160e164d68
Bidur <bidur_subedi@yahoo.com>
2013-11-26 19:34:16 +0545

    Function to Check room status
    Function to check the status of a room on a given date

--- application/models/rooms.php
+++ application/models/rooms.php
@@ -2,10 +2,43 @@
 
 class Rooms extends CI_Model{
 
+	/*Function to change password of given user
+	  User ID and new passwoed should be provided
+	  Does not check for any validity
+	  By: Bidur Subedi
+	  Nov 24, 2013 */
 	public function change_password($id,$credentials){
 		$this->db->where('user_id',$id);
 		$this->db->update('user',$credentials);
 	}
+
+	/*Function to obtain the status of room
+	  Room ID and the date  to check status should be supplied
+	  By: Bidur Subedi
+	  Nov 25, 2013 */
+	public function get_status($roomID,$date){
+		//Obtain all booking with that room ID
+		$this->db->select('booking_id');
+		$this->db->where('room_id',$roomID);
+		$booking_id=$this->db->get('booking_room');
+		$booking_id=$booking_id->result();
+		$book_id = array();
+		foreach ($booking_id as $value) {
+			array_push($book_id, $value->booking_id);	
+		}
+		if ($book_id == array())
+			return 0;
+		$this->db->select('status');
+		$this->db->where_in('booking_id',$book_id);
+		$this->db->where('checkin_date <=', $date);
+		$this->db->where('checkout_date >=', $date);
+		$result=$this->db->get('booking')->result();
+		if($result == array())
+			return 0;
+		$result=$result[0]->status;
+		return $result;
+	}
+
 
 }
 
