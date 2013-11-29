0841d082993f22e3d09a88a146fe4ea87771cbaf
bibekkc <dothemuzic@gmail.com>
2013-11-29 16:23:20 +0545

    customized edit page.

--- application/views/edit.php
+++ application/views/edit.php
@@ -14,111 +14,173 @@
 					</div> <!-- ends:breadcrums -->
 					
 					<!--form div starts here-->
-					<div>
-						<form action="edit_update" method="post" enctype="multipart/form-data">
-							<fieldset>
-								<label>Hotel Name</label><br />
-								<?php echo $hotel_details[0]->name;?>
-							</fieldset><br />
-							<fieldset>
-								<label>Address</label><br />
-								<input type="text" name="address" id="address" value="<?php echo $hotel_details[0]->address;?>" />
-							</fieldset><br />
-							<fieldset>
-								<label>City</label><br />
-								<input type="text" name="city" id="city" value="<?php echo $hotel_details[0]->city;?>" />
-							</fieldset><br />
-							<fieldset>
-								<label>Phone1</label><br />
-								<input type="text" name="phone1" id="phone1" value="<?php echo $hotel_details[0]->phone1;?>" />
-							</fieldset><br />
-							<fieldset>
-								<label>Phone2</label><br />
-								<input type="text" name="phone2" id="phone2" value="<?php echo $hotel_details[0]->phone2;?>" />
-							</fieldset><br />
-							<fieldset>
-								<label>E-mail</label><br />
-								<input type="text" name="email" id="email" value="<?php echo $hotel_details[0]->email; ?>" />
-							</fieldset><br />
-							<fieldset>
-								<label>URL</label><br />
-								<input type="text" name="url" id="url" value="<?php echo $hotel_details[0]->url; ?>" />
-							</fieldset><br />
-							<fieldset>
-								<label>Google Map URL</label><br />
-								<input type="text" name="google_url" id="google_url" value="<?php echo $hotel_details[0]->google_map_url; ?>" />
-							</fieldset><br />
-							<fieldset>
-								<label>Default Image</label><br />
-								<input type="file" name="default_image" id="default_image" disabled="disabled" onChange="readURL(this);" />
-								
-								<?php
-									if($hotel_details[0]->default_imgid==0){
-								?>
-								
-								<br /><img  style="display:none;" id="img_prev" src="#" alt="Image Preview" />
-								<?php 
-									}else{
-							 	?>
-							 	<img src="<?php echo $default_image[0]->path; ?>" id="img_prev" alt="<?php echo $default_image[0]->alt; ?>" width="150px" height="150px" />
-							 	<?php 
-							 		} 
-							 	?>
-							</fieldset><br />
-							<fieldset>
-								<label>Other Images</label><br />
-								<input type="file" name="images" id="images" disabled="disabled" />
-								<?php
-									if($hotel_details[0]->default_imgid==''){
-								?>
-								<br/><div id="image_display"></div><br />
-								<?php
-									}else{
-										for($i=0;$i<sizeof($other_image);$i++){
-								?>
-										<img src="<?php echo $other_image[$i]->path; ?>" alt="<?php echo $other_image[$i]->alt; ?>" width="150px" height="150px" />
-								<?php 
-										}
-									}
-								?>
-							</fieldset><br />
-							<fieldset>
-								<label>Description</label><br />
-								<textarea name="description" id="description" value="<?php echo $hotel_details[0]->description; ?>"></textarea>
-							</fieldset><br />
-							<fieldset>
-								<label>Facilities</label><br />
-								<?php 
-									$size = sizeof($hotel_facilities);
-									$sz = $size-1;
-									$row = intval($sz/6);
-									$j=0;
-									$count = 0;
-									for($i=0;$i<$row;$i++){
-										while($j<$size){
-											
+					<div class=" row edit-update-wrap"> <!-- starts:edit update wrap -->
+						<div class="edit-update">
+							<div class="col-md-12">
+								<form action="edit_update" method="post" enctype="multipart/form-data">
+							
+								<div class="edit-update-head">
+									<?php echo $hotel_details[0]->name;?>
+								</div>
+								<div class="form-content">
+									<div class="row form-row">  <!-- starts:form-row -->
+										<div class="col-sm-4 col-xs-4 col-md-4">
+											<label>Address</label>
+										</div>
+										<div class="col-sm-8 col-xs-8 col-md-6">
+											<input class="form-control" type="text" name="address" id="address" value="<?php echo $hotel_details[0]->address;?>" />
+										</div>
+									</div> <!-- ends:form-row -->
 
-											$checked = '';
-											if($hotel_facilities[$j][2]=='1'){
-												$checked='checked';
+									<div class="row form-row">  <!-- starts:form-row -->
+										<div class="col-sm-4 col-xs-4 col-md-4">
+											<label>Phone1</label>
+										</div>
+										<div class="col-sm-8 col-xs-8 col-md-6">
+											
+											<input class="form-control" type="text" name="phone1" id="phone1" value="<?php echo $hotel_details[0]->phone1;?>" />
+										</div>
+									</div> <!-- ends:form-row -->
+
+									<div class="row form-row">  <!-- starts:form-row -->
+										<div class="col-sm-4 col-xs-4 col-md-4">
+											<label>Phone2</label>
+										</div>
+										<div class="col-sm-8 col-xs-8 col-md-6">
+											<input class="form-control" type="text" name="phone2" id="phone2" value="<?php echo $hotel_details[0]->phone2;?>" />
+										</div>
+									</div> <!-- ends:form-row -->
+
+									<div class="row form-row">  <!-- starts:form-row -->
+										<div class="col-sm-4 col-xs-4 col-md-4">
+											<label>URL</label>
+										</div>
+										<div class="col-sm-8 col-xs-8 col-md-6">
+											<input class="form-control" type="text" name="url" id="url" value="<?php echo $hotel_details[0]->url; ?>" />
+										</div>
+									</div> <!-- ends:form-row -->
+
+									<div class="row form-row">  <!-- starts:form-row -->
+										<div class="col-sm-4 col-xs-4 col-md-4">
+											<label>Google Map URL</label>
+										</div>
+										<div class="col-sm-8 col-xs-8 col-md-6">
+											<input class="form-control" type="text" name="google_url" id="google_url" value="<?php echo $hotel_details[0]->google_map_url; ?>" />
+										</div>
+									</div> <!-- ends:form-row -->
+
+									<div class="row form-row">  <!-- starts:form-row -->
+										<div class="col-sm-4 col-xs-4 col-md-4">
+											<label>Default Image</label>
+										</div>
+										<div class="col-sm-8 col-xs-8 col-md-8">
+											<input type="file" name="default_image" id="default_image" onChange="readURL(this);" />
+											<?php
+												if($hotel_details[0]->default_imgid==0){
+											?>
+											
+											<br /><img  style="display:none; padding:4px; border:1px solid #c7c7c7; border-radius: 5px;" id="img_prev" src="#" alt="Image Preview" />
+											<?php 
+												}else{
+										 	?>
+										 	<img src="<?php echo $default_image[0]->path; ?>" id="img_prev" alt="<?php echo $default_image[0]->alt; ?>" width="150px" height="150px" />
+										 	<?php 
+										 		} 
+										 	?>
+										</div>
+									</div> <!-- ends:form-row -->
+
+									<div class="row form-row">  <!-- starts:form-row -->
+										<div class="col-sm-4 col-xs-4 col-md-4">
+											<label>Other Images</label>
+										</div>
+										<div class="col-sm-8 col-xs-8 col-md-8">
+											<input type="file" name="images" id="images" onChange="showimg(this);"/>
+											<?php
+												if($hotel_details[0]->default_imgid==''){
+											?>
+											<br/><div id="image_display"></div><br />
+											<?php
+												}else{
+													for($i=0;$i<sizeof($other_image);$i++){
+											?>
+													<img src="<?php echo $other_image[$i]->path; ?>" alt="<?php echo $other_image[$i]->alt; ?>" width="150px" height="auto" />
+											<?php 
+													}
+												}
+											?>
+										</div>
+									</div> <!-- ends:form-row -->
+
+									<div class="row form-row">  <!-- starts:form-row -->
+										<div class="col-sm-4 col-xs-4 col-md-4">
+											<label>Description</label>
+										</div>
+										<div class="col-sm-8 col-xs-8 col-md-6">
+											<textarea class="form-control" name="description" id="description" value="<?php echo $hotel_details[0]->description; ?>"></textarea>
+										</div>
+									</div> <!-- ends:form-row -->
+
+									<div class="row">
+										<div class="col-md-12">
+											<label>Facilities</label>
+										</div>
+
+										<?php 
+											$size = sizeof($hotel_facilities);
+											$sz = $size-1;
+											$row = intval($sz/6);
+											$j=0;
+											$count = 0;
+											for($i=0;$i<$row;$i++){
+												while($j<$size){
+													
+
+													$checked = '';
+													if($hotel_facilities[$j][2]=='1'){
+														$checked='checked';
+													}
+										?>
+										<div class="col-md-4">
+											<label><input type="checkbox" name="check[]" value="<?php echo $hotel_facilities[$j][0]; ?>" <?php echo $checked; ?> /><?php echo $hotel_facilities[$j][1];?></label>
+										</div>
+
+										<?php
+													$j++;
+													$count++;
+													if($count==6){
+														$count = 0;
+														echo '<br />';
+														break;
+													}
+												}
 											}
-								?>
-								 			<input type="checkbox" name="check[]" value="<?php echo $hotel_facilities[$j][0]; ?>" <?php echo $checked; ?> /><label><?php echo $hotel_facilities[$j][1];?></label>&nbsp;&nbsp;
-								<?php
-											$j++;
-											$count++;
-											if($count==6){
-												$count = 0;
-												echo '<br />';
-												break;
-											}
-										}
-									}
-								?>
-							</fieldset><br />
-							<input type="submit" name="update" id="update" value="Update" class="btn btn-default inn-button" />
+										?>
+										
+									</div>
+
+									
+										
+										
+									
+									
+										
+										
+									
+									
+										
+										
+									<div class="row">
+										<div class="col-md-12">
+											<input type="submit" name="update" id="update" value="Update" class="btn btn-default" />
+										</div>
+									</div>
+								</div>
+								<div class="clear"></div>
 						</form>
-					</div><!--form div ends here-->
+							</div>
+						</div>
+					</div><!-- ends:edit update wrap -->
 
 				</div>
 			</div><!-- Ends:row -->
